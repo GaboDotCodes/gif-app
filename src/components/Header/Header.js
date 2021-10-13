e8c90f85209fd846757606698b511ca620240e89
GaboDotCodes <gabedu2000@gmail.com>
2021-10-12 21:39:28 -0500

    Header and footer

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import './Header.css';
+import 'animate.css';
+const Header = () => {
+    return (
+        <div className="header">
+            <img src="https://media3.giphy.com/media/wHVa06cMnppBS18j1e/giphy.gif" alt="First rabbit dancing" className="header-gif"/>
+            <h1 className="header-title ">Have fun!</h1>
+            <img src="https://media1.giphy.com/media/CPy1YwYXatcW8HTLO4/giphy.gif" alt="Second rabbit dancing" className="header-gif"/>
+        </div>
+    )
+}
+
+export default Header
